b1d61fce1db43111458d799e8a3a173fe66e25ee
Archan Patel <archan.patel890@gmail.com>
2017-10-29 18:52:46 -0400

    Added interfaces and some implementation for added Players to MySQL.
    Fixed bug with timezone and componentScan.
    
    Example program of adding and retrieving players with mysql is functional
    
    Fixed bug with incompatible SQL dialect.
    
    Fixed error with Service Cast exception. Added controller for get players.
    
    Added endpoints for creating player and getting players.

---
+++ backend/src/main/java/com/fantasystocks/app.java
@@ -0,0 +1,14 @@
+package com.fantasystocks;
+
+import com.fantasystocks.config.AppConfig;
+import com.fantasystocks.entity.Player;
+import com.fantasystocks.service.model.PlayerService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class app {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
---
+++ backend/src/main/java/com/fantasystocks/entity/Player.java
@@ -0,0 +1,47 @@
+package com.fantasystocks.entity;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@Entity
+@Table(name = "Players")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Player {
+    private static final int MAX_USER_CHARACTERS = 30;
+    private static final int MIN_USER_CHARACTERS = 5;
+    private static final String USER_NAME_ERROR_MSG =
+            "Username must be between " + MIN_USER_CHARACTERS + " and " + MAX_USER_CHARACTERS + ".";
+    private static final int MAX_NAME_CHARACTERS = 30;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PID")
+    private long id;
+
+    @NonNull
+    @Column(name = "first_name")
+    @Size(max = MAX_NAME_CHARACTERS, message = "Name is too long.")
+    private String firstName;
+
+    @NonNull
+    @Column(name = "last_name")
+    @Size(max = MAX_NAME_CHARACTERS, message = "Name is too long.")
+    private String lastName;
+
+    @NonNull
+    @Column(name = "user_name", unique = true)
+    @Size(max = MAX_USER_CHARACTERS, min = MIN_USER_CHARACTERS, message = USER_NAME_ERROR_MSG)
+    private String username;
+
+    @NonNull
+    @Column(name = "email", unique = true)
+    @Email
+    private String email;
+}
