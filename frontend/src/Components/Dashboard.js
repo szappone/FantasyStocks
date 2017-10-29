b9781bcaad95bac5ec81b41c8a19487fdec93daa
Austin Kemper <austin.kemper@live.com>
2017-10-29 18:49:17 -0400

    get dashboard data from service

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -8,18 +8,36 @@ const API_PREFIX = "http://localhost:8080";
 
 class Dashboard extends Component {
 
-  constructor() {
-    super();
-    this.state = {sessions: []};
+  constructor(props) {
+    super(props);
+    this.setSessionDataInState = this.setSessionDataInState.bind(this);
+    this.state = {
+      sessions: ["a", "b", "c"],
+      handle: "unknown"
+  };
+    console.log("logging props from constructor");
+    console.log(this.props);
 
   }
 
-  componentWillReceiveProps() {
-    console.log(this.props.globalService);
-    this.setState({sessions: this.props.globalService.getSessions()});
+  componentWillMount(){
+    this.setSessionDataInState();
+  }
+
+  setSessionDataInState() {
+    if (this.props && this.props.globalService) {
+      this.setState({sessions: this.props.globalService.getSessions()});
+      this.setState({handle: this.props.globalService.handle});
+      console.log(this.props.globalService.handle);
+      console.log("successfully set our state from global service");
+    } else {
+      console.log("cant access global service yet!");
+    }
   }
 
   render() {
+    let currHandle = this.state.handle;
+    console.log("curr Handle: " + currHandle);
 
     return (
 
@@ -27,36 +45,24 @@ class Dashboard extends Component {
 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+<<<<<<< HEAD
             <h1 className="App-title">  Welcome to the dashboard!</h1>
+=======
+            <h1 className="App-title">Welcome to the dashboard {currHandle}!</h1>
+>>>>>>> get dashboard data from service
         </header>
 
 
         <h2> Your Sessions </h2>
 
 
+        {console.log("our sessions: " + this.state.sessions)}
+        {console.log("our handle: " + currHandle)}
+          {this.state.sessions.map((session) => (
+              <p>Session # {session.sessionId} . </p>
+          ))}
 
-        {this.state.sessions.map((session) => (
-            <p>Session # {session.sessionId}</p>
-        ))}
-
-{
-/*
-
-        <button className="Dash-button">
-            <Link to='/Session'>Session</Link>
-        </button><br/>
-
-        <button className="Dash-button">
-          Enter Session 2
-        </button><br/>
-
-        <button className="Dash-button">
-          Enter Session 3
-        </button>
-*/
-}
-
-  </div>
+      </div>
 
     );
   }
