73c2009974e4597b842ee3929dca451c47780483
Austin Kemper <austin.kemper@live.com>
2017-11-18 14:47:31 -0600

    add deployment script and document in readme

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install mysql-server
+sudo apt install npm
+sudo apt install maven
+sudo apt install openjdk-8-jdk
+sudo /etc/init.d/mysql start
+
+# Make sure node and npm up to date by installing nvm first
