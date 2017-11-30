633dfd2c415dc7dd5119eaf02296b2946856541c
ShivKrishnan96 <skrish23@jhu.edu>
2017-11-30 15:53:10 -0500

    Tournament table is now a list of Matchup IDs

--- backend/src/main/java/com/fantasystocks/modules/RobinRound.java
+++ backend/src/main/java/com/fantasystocks/modules/RobinRound.java
@@ -1,5 +1,6 @@
 package com.fantasystocks.modules;
 
+import com.fantasystocks.entity.Matchup;
 import java.util.*;
 
 public class RobinRound {
@@ -8,14 +9,13 @@ public class RobinRound {
 
         String[] players = {"Shiv","AK","Archan","Sarah","Angelo","Scott","Passumarthi","Krishnan","Adam","P"};
 
-        String[][][] matchup = RoundRobin(players);
+        String[][][] matchups = RoundRobin(players);
+        Matchup[] tournaments = createMatchupIDs(matchups);
 
-        for (int i = 0; i < players.length - 1; i++) {
-            for (int j = 0; j < players.length/2; j ++ ) {
-                System.out.print(matchup[i][j][0] + " ");
-                System.out.print(matchup[i][j][1] + " | ");
-            }
-            System.out.println();
+        for (int i = 0; i < 45; i++) {
+            System.out.print(tournaments[i].getPlayer1Name() + " ");
+            System.out.print(tournaments[i].getPlayer2Name() + " ");
+            System.out.println(tournaments[i].getActiveWeek());
         }
 
     }
@@ -52,5 +52,21 @@ public class RobinRound {
             }
         }
         return matchup;
+    }
+    public static Matchup[] createMatchupIDs (String[][][] players){
+        Matchup[] tournament = new Matchup[45];
+        int k = 0;
+        for (int i = 0; i < 9; i ++) {
+            for (int j = 0; j < 5; j++) {
+                Matchup m = new Matchup();
+                m.setPlayer1Name(players[i][j][0]);
+                m.setPlayer2Name(players[i][j][1]);
+                m.setActiveWeek(i+1);
+                tournament[k] = m;
+                k++;
+            }
+
+        }
+        return tournament;
     }
 }
